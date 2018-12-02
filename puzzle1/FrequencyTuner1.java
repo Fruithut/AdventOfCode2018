e1be9c7bbd3852b5286af5ad6c42e9b22bf16c40
Fruithut <fruithut@tutanota.com>
2018-12-02 19:51:04 +0100

    [DAY 1] Refactored solutions

--- puzzle1/FrequencyTuner1.java
+++ puzzle1/FrequencyTuner1.java
@@ -3,13 +3,6 @@ import java.io.File;
 import java.io.FileReader;
 
 public class FrequencyTuner1 {
-
-    public static int applyMathOperation(String freqChange, int baseFreq) {
-        String operator = freqChange.substring(0, 1);
-        int value = Integer.valueOf(freqChange.substring(1, freqChange.length()));
-        if (operator.equals("+")) return baseFreq + value;
-        return baseFreq - value;
-    }
     
     public static void main(String[] args) {
         int changingFreq = 0;
@@ -17,7 +10,7 @@ public class FrequencyTuner1 {
         try (BufferedReader reader = new BufferedReader(new FileReader("./puzzle1/input.txt"));) {
             String input = reader.readLine();
             while (input != null) {
-                changingFreq = applyMathOperation(input, changingFreq);
+                changingFreq += Integer.valueOf(input);
                 input = reader.readLine();
             }
         } catch (Exception e) {
--- puzzle1/FrequencyTuner2.java
+++ puzzle1/FrequencyTuner2.java
@@ -5,13 +5,6 @@ import java.util.Map;
 
 public class FrequencyTuner2 {
 
-    public static int applyMathOperation(String freqChange, int baseFreq) {
-        String operator = freqChange.substring(0, 1);
-        int value = Integer.valueOf(freqChange.substring(1, freqChange.length()));
-        if (operator.equals("+")) return baseFreq + value;
-        return baseFreq - value;
-    }
-
     public static void main(String[] args) {
         Map<Integer, Integer> countMap = new HashMap<>();
         int changingFreq = 0;
@@ -19,21 +12,20 @@ public class FrequencyTuner2 {
 
         try (BufferedReader reader = new BufferedReader(new FileReader("./puzzle1/input.txt"));) {
             String input = "";
-            reader.mark(4000);
+            reader.mark(5000);
             while (true) {
                 input = reader.readLine();
-                // reset after reaching end of file
                 if (input == null) {
                     reader.reset();
                     continue;
                 }
 
-                changingFreq = applyMathOperation(input, changingFreq);
-
+                changingFreq += Integer.valueOf(input);
                 int timesSpotted = countMap.containsKey(changingFreq) ? (countMap.get(changingFreq) + 1) : 1;
-                countMap.put(changingFreq, timesSpotted);
-
+                // We've found the repeating frequency
                 if (timesSpotted == 2) break;
+
+                countMap.put(changingFreq, timesSpotted);
             }
         } catch (Exception e) {
             e.printStackTrace();
