a87b6b2245345f1bac46e269542b9a569acc89d8
Olav <fruithut@tutanota.com>
2018-12-04 05:14:06 +0100

    [DAY 3] Added solution to first problem

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Solutions to [Advent Of Code 2018](https://adventofcode.com/)
 |-----|------|----------|
 |[1: Chronal Calibration](https://adventofcode.com/2018/day/1)|[1](/puzzle1/FrequencyTuner1.java) [2](/puzzle1/FrequencyTuner2.java)|Java|
 |[2: Inventory Management System](https://adventofcode.com/2018/day/2)|[1](/puzzle2/InventoryChecksum.py) [2](/puzzle2/SimilarPackages.py)|Python|
-|[3: No Matter How You Slice It](https://adventofcode.com/2018/day/3)|..|Python|
+|[3: No Matter How You Slice It](https://adventofcode.com/2018/day/3)|[1+..](/puzzle3/OverlapChecker.py)|Python|
--- puzzle3/OverlapChecker.py
+++ puzzle3/OverlapChecker.py
@@ -1,13 +1,13 @@
 from collections import defaultdict
+import numpy
 
-def getInput(filePath: str):
+def getInput(file_path: str):
     file_content = []
-    for string in open(filePath, 'r', 1):
+    for string in open(file_path, 'r', 1):
         values = string.split(" ")
         first_pair = values[2].split(',')
         second_pair = values[3].split('x')
-        file_content.append({
-            'id': int(values[0][1:]), 
+        file_content.append({ 
             'x_offset': int(first_pair[0]), 
             'y_offset': int(first_pair[1][:-1]), 
             'width': int(second_pair[0]), 
@@ -15,7 +15,28 @@ def getInput(filePath: str):
         })
     return file_content
 
-square_input = getInput('input.txt')
+def generateHitMatrix(claim_specifications: [{}]):
+    claim_matrix = numpy.zeros((1000,1000))
+    for line in claim_specifications:
+        for x in range(line['x_offset'], line['x_offset'] + line['width']):
+            for y in range(line['y_offset'], line['y_offset'] + line['height']):
+                claim_matrix[x][y] += 1
+    return claim_matrix
 
-square_inches = defaultdict(list)
+def countMultipleClaims(claim_matrix: numpy.ndarray):
+    claim_hits = 0
+    for i in claim_matrix:
+        for j in i:
+            if j > 1:
+                claim_hits += 1
+    return claim_hits
+
+# PART ONE
+contents = getInput('input.txt')
+hit_matrix = generateHitMatrix(contents)
+hits = countMultipleClaims(hit_matrix)
+print(hits)
+
+# PART TWO
+
     
