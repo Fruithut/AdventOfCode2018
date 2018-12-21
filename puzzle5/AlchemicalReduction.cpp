527434f9acaf76395842d2068a0e8d87605fa082
Olav Gjerde <fruithut@tutanota.com>
2018-12-21 03:49:12 +0100

    [DAY 5] Set "using namespace std" for easier reading of sourcecode

--- puzzle5/AlchemicalReduction.cpp
+++ puzzle5/AlchemicalReduction.cpp
@@ -1,20 +1,19 @@
 #include <fstream>
 #include <iostream>
 #include <stack>
-#include <vector>
-#include <map>
 #include <limits>
+using namespace std;
 
-std::stack<char> getInputToStack(std::string path) {
-    std::stack<char> charStack;
+stack<char> getInputToStack(string path) {
+    stack<char> charStack;
     char next;
-    std::fstream fin(path, std::fstream::in);
+    fstream fin(path, fstream::in);
     while (fin >> next) charStack.push(next);
     return charStack;
 }
 
-std::stack<char> reduce(std::stack<char>& polymer) {
-    std::stack<char> reducedStack;
+stack<char> reduce(stack<char>& polymer) {
+    stack<char> reducedStack;
     while (!polymer.empty()) {
         if (reducedStack.empty()) {
             reducedStack.push(polymer.top());
@@ -26,7 +25,7 @@ std::stack<char> reduce(std::stack<char>& polymer) {
         char next = polymer.top();
         polymer.pop();
 
-        bool equals = char(std::tolower(previous)) == char(std::tolower(next));
+        bool equals = char(tolower(previous)) == char(tolower(next));
         bool differentCase = previous < next || previous > next;
 
         if (!(equals && differentCase)) {
@@ -37,37 +36,37 @@ std::stack<char> reduce(std::stack<char>& polymer) {
     return reducedStack;
 }
 
-std::stack<char> firstReduction(std::stack<char>& initialStack) {
-    std::stack<char> reduced = reduce(initialStack);
-    std::cout << "Units first reduction: " << reduced.size() << std::endl;
+stack<char> firstReduction(stack<char>& initialStack) {
+    stack<char> reduced = reduce(initialStack);
+    cout << "Units first reduction: " << reduced.size() << endl;
     return reduced;
 }
 
-std::stack<char> secondReduction(std::stack<char>& firstStepStack) {
-    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
-    int minimalSize = std::numeric_limits<int>::max();
-    std::stack<char> finalStack;
+stack<char> secondReduction(stack<char>& firstStepStack) {
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    int minimalSize = numeric_limits<int>::max();
+    stack<char> finalStack;
 
     for (char alpha : alphabet) {
-        std::stack<char> polyCopy = firstStepStack;
+        stack<char> polyCopy = firstStepStack;
 
         //Filter step
-        std::stack<char> filteredPoly;
+        stack<char> filteredPoly;
         while (!polyCopy.empty()) {
             char current = polyCopy.top();
             polyCopy.pop();
-            if (!(alpha == std::tolower(current))) {
+            if (!(alpha == tolower(current))) {
                 filteredPoly.push(current);
             }
         }
         //Find best reduction
-        std::stack<char> finalStack = reduce(filteredPoly);
+        stack<char> finalStack = reduce(filteredPoly);
         if (finalStack.size() < minimalSize) {
             minimalSize = finalStack.size();
             finalStack = filteredPoly;
         }
     }
-    std::cout << "Units after second reduction: " << minimalSize;
+    cout << "Units after second reduction: " << minimalSize;
     return finalStack;
 }
 
